1190bc91520b062e6139b93c3acf1a7f998c878b
Ryan Phan <ryanphan11@gmail.com>
2025-07-21 17:18:40 -0700

    Update to include @State and constants

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+    let youAreAweSomeMessage = "You are Awesome!"
+    let youAreGreatMessage = "You are Great!"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text(message)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            Spacer()
+            Button("Press Me") {
+                if imageName == "hand.thumbsup" {
+                    imageName = "sun.max.fill"
+                    message = youAreAweSomeMessage
+                } else {
+                    imageName = "hand.thumbsup"
+                    message = youAreGreatMessage
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.orange)
         }
         .padding()
     }
